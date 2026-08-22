6cb429b83b0505d7ee22dfa72ec02f65cc990a5d
NaveenMR-beep <mrn748305@gmail.com>
2026-08-22 23:57:25 +0530

    Enable HTTPS Basic SSL dev server

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    basicSsl(), // Automatically generates self-signed SSL cert for secure context on phone
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg'],
